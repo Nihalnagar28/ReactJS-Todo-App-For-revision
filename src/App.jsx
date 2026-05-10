0c14a4e41f08eaa236809ce99e3b7eaa1d21b175
Nihal <nihalop28@gmail.com>
2026-05-10 12:15:32 +0530

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
-import { useState } from 'react'
+import { useActionState, useState } from 'react'
 import Navbar from './components/Navbar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleEdit=()=>{
+
+  }
+  const handleDelete=()=>{
+
+  }
+  const handleAdd=()=>{
+    setTodos([...todos,{todo,isCompleted:false}])
+    setTodo("")
+  }
+
+  const handleChange=(e)=>{
+    setTodo(e.target.value)
+  }
 
   return (
     <>
-      <Navbar></Navbar>
+    <Navbar/>
    <div className="container mx-auto my-5 rounded-xl p-5 w-300 bg-violet-100 min-h-[80vh]">
     <div className="addTodo my-5">
       <h2 className='text-lg font-bold '>Add a Todo</h2>
-      <input type="text" className='bg-white w-1/2'/>
+      <input type="text" onChange={handleChange} value={todo} className='bg-white w-1/2'/>
       <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 text-sm py-1 font-bold text-white rounded-md mx-6'>Add</button>
    </div>
       <h2 className='text-lg font-bold'>Your TODOS</h2>
       <div className="todos">
-        <div className="todo flex">
-          <div className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, quod?</div>
+        {todos.map(item=>{
+
+         return <div key={todo} className="todo flex w-1/4 justify-between">
+          <div className={item.isCompleted?"":"line-through"}>{item.todo} </div>
           <div className="buttons">
             <button className='bg-violet-800 hover:bg-violet-950 p-2 text-sm py-1 font-bold text-white rounded-md mx-1' onClick={handleEdit}>Edit</button>
             <button className='bg-violet-800 hover:bg-violet-950 p-2 text-sm py-1 font-bold text-white rounded-md mx-1' onClick={handleDelete}>Delete</button>
           </div>
         </div>
+        })}
       </div>
     </div>
     </>
